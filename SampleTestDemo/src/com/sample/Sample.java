d435d5cd63bc9cb4c4d0fd072aeb1051c5d47c0c
kalyanidr <kalyanidurgaravindra@gmail.com>
2024-03-18 00:01:04 +0530

    sample project added

---
+++ SampleTestDemo/src/com/sample/Sample.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is sample project.");
+	}
+	
+	
+
+	
+}
---
+++ SampleTestDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author udaan
+ *
+ */
+module SampleTestDemo {
+}
