7626465c3a38d26c5309746f0c8ea0380cf08621
kalyanidr5 <kalyanidurgaravindra@gmail.com>
2024-03-20 23:05:11 +0530

    new line added

--- SampleTestDemo/src/com/sample/Sample.java
+++ SampleTestDemo/src/com/sample/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("This is sample project. ");
+		System.out.println("This is a newly added line.");
 	}
 	
 	
